c1e51d7c74ffe5cf6a25601a48f8210fea8b6013
ErickFSilva <erick_ferreira@outlook.com>
2024-12-06 16:47:40 -0300

    javascript-typescript, 06/12/2024(tarde)

---
+++ javascript-typescript/secao-02-para-iniciantes/aula017-let-var-primeira-diferenca.js
@@ -0,0 +1,21 @@
+/**
+ * As variáveis declaradas com 'var' podem ser redeclaradas.
+ */
+
+var nome = 'Erick';
+console.log(nome);
+
+var nome = 'João';
+console.log(nome);
+
+variavel = "Não crie variável assim!!!";
+console.log(variavel);
+
+
+// ECMAScript (ES6) -> Padronização do JavaScript
+/**
+ * As variáveis declaradas com 'let' não podem ser redeclaradas
+ */
+
+let usuario = 'Erick';
+console.log(usuario);
